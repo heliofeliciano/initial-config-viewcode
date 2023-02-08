0d74ba53e371dae1cc29e53113dfc87db09c0380
heliofeliciano <heliofeliciano@gmail.com>
2023-02-07 21:04:35 -0300

    Initial Commit

---
+++ initial-config-viewcode/initial-config-viewcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  initial-config-viewcode
+//
+//  Created by Helio Feliciano on 07/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
